d51668299a5ad03c31aac9b7abbf546d12014231
rodriguezhr25 <rodriguezhr25@gmail.com>
2020-04-10 23:41:04 -0400

    project

--- js/salmonriver.js
+++ js/salmonriver.js
@@ -1,5 +1,11 @@
 const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
+var today = new Date();
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+
+document.getElementById('dateUpdated').textContent = days[today.getDay()] + ", "  + " " +today.getDate() +" " + months[today.getMonth()]+ " " + today.getFullYear();
 
 function toggleMenu() {
     document.querySelector(".navigation").classList.toggle("responsive");
